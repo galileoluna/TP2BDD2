048debd242d93058bc02b347427fc63e8502ba32
Galileo Luna <galileoungs@gmail.com>
2019-06-10 03:54:19 +0000

    funciona

--- registrar.php
+++ registrar.php
@@ -2,38 +2,35 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Documento sin título</title>
+<title>Registro</title>
 </head>
 
 <body>
 
 <?php
 
-	$usuario= $_POST["usu"];
-	$contrasenia= $_POST["contra"];
+	
+	$apellido= $_POST["apel"];
+	$nombre= $_POST["nom"];
+	$alias= $_POST["al"];
+	$clave= $_POST["clav"];
 	
 	
 				
 	try{
+		$dbconn = pg_connect("host=localhost dbname=pelispedia user=postgres password=root")
+    or die('No se ha podido conectar: ' . pg_last_error());
 
-		$base=new PDO('mysql:host=localhost; dbname=pruebas', 'root', '');
-		
-		$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
-		$base->exec("SET CHARACTER SET utf8");		
-		
-		
-		$sql="INSERT INTO USUARIOS (USUARIOS, PASSWORD) VALUES (:usu, :contra)";
-		
-		$resultado=$base->prepare($sql);		
-		
-		
-		$resultado->execute(array(":usu"=>$usuario, ":contra"=>$contrasenia));		
-		
-		
-		echo "Registro insertado";
-		
-		$resultado->closeCursor();
+// Realizando una consulta SQL
+$query="INSERT INTO USUARIO(usuario_id,apellido,nombre,alias,clave) values(default, '$apellido','$nombre','$alias','$clave')";
+$result = pg_query($dbconn, $query) or die('ERROR AL INSERTAR DATOS: ' . pg_last_error());
+$cmdtuples = pg_affected_rows($result);
+echo $cmdtuples . " datos registrados.\n";
+// Free resultset liberar los datos
+pg_free_result($result);
+
+// Closing connection cerrar la conexión
+pg_close($dbconn);
 
 	}catch(Exception $e){			
 		
